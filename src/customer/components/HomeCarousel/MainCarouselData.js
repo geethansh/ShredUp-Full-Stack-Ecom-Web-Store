5c0ca43a46410823473cdd281164cef07951eacb
Geethansh <geethansh@gmail.com>
2023-12-03 00:42:28 +0530

    Demo Commit

---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,22 @@
+export const MainCarouselData=[
+{
+    image:"https://images.unsplash.com/photo-1571902943202-507ec2618e8f?w=3000&h=1000&auto=format&fit=clip&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3ltfGVufDB8MHwwfHx8MA%3D%3D",
+    path:"/women/clothing/Tops"
+},
+{
+    image:"https://images.unsplash.com/photo-1521805103424-d8f8430e8933?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    path:""
+},
+{
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1mZRGp20lojCUgoGP0eDujXVB0Id7Mu1HBw&usqp=CAU",
+    path:""
+},
+{
+    image:"src\customer\components\HomeCarousel\victor-freitas-Yuv-iwByVRQ-unsplash.jpg",
+    path:""
+}
+//find good images
+
+
+
+]
